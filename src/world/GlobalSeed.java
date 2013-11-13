8f3534c9c894a15832990c6297a93bbed6f361b4
Borja Molina Zea <borja.mz8@gmail.com>
2013-11-13 13:03:41 +0100

    Added the class GlobalSedd

---
+++ src/world/GlobalSeed.java
@@ -0,0 +1,22 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Borja Molina Zea.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl.html
+ * 
+ * Contributors:
+ *     Borja Molina Zea - initial API and implementation
+ ******************************************************************************/
+/**
+ * All the 'random' numbers used for create the world.
+ * Starting from a seed-string, like "hola", it generates 2^18 different numbers.
+ * Different strings generates different numbers.
+ * The numbers are stored in a integer array.
+ * v 0.0
+ */
+package world;
+
+public class GlobalSeed {
+
+}
