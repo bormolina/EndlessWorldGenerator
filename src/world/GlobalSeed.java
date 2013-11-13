85e7bec611ad438dca04c46a71c5aa8dc3ae76aa
Borja Molina Zea <borja.mz8@gmail.com>
2013-11-13 15:14:18 +0100

    version .1 of GlobalSeed, NOT TESTED YET

--- src/world/GlobalSeed.java
+++ src/world/GlobalSeed.java
@@ -13,10 +13,61 @@
  * Starting from a seed-string, like "hola", it generates 2^18 different numbers.
  * Different strings generates different numbers.
  * The numbers are stored in a integer array.
- * v 0.0
+ * v 0.1
  */
 package world;
 
-public class GlobalSeed {
+import java.io.IOException;
 
+public class GlobalSeed {
+	public String sSeed;
+	public int nSeed;
+	public int size = 262144;
+	public int amplitudeIndex = 0;
+	public int amplitudeSize = 128;
+	public int frecuencyIndex = amplitudeIndex + amplitudeSize;
+	public int frecuencySize = 128;
+	public int xPhaseIndex = frecuencyIndex + frecuencySize;
+	public int xPasheSize = 128;
+	public int zPhaseIndex = xPhaseIndex + xPasheSize;
+	public int zPhaseSize = 128;
+
+	public int[] seeds = new int[size] ;
+		
+	public GlobalSeed(String seed){
+		this.sSeed = seed;
+		this.nSeed = this.generateNumberSeed();
+	}
+	
+	private int generateNumberSeed(){
+		return this.sSeed.hashCode();
+	}
+	
+	public int getGlobal(int index){
+		return this.seeds[index];
+	}
+	
+	public int getAmplitude(int index){
+		index%=this.amplitudeSize;
+		index+=this.amplitudeIndex;
+		return this.seeds[index];
+	}
+	
+	public int getFrecuency(int index){
+		index%=this.frecuencySize;
+		index+=this.frecuencyIndex;
+		return this.seeds[index];
+	}
+
+	public int xPhase(int index){
+		index%=this.frecuencySize;
+		index+=this.frecuencyIndex;
+		return this.seeds[index];
+	}
+	
+	public int zPhase(int index){
+		index%=this.zPhaseSize;
+		index+=this.zPhaseIndex;
+		return this.seeds[index];
+	}
 }
